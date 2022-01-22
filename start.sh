c29e380ff447db46c97858476e200186ec976635
jcardarelli <john@cardarel.li>
2022-01-22 15:02:07 -0800

    Add startup script

--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
-uvicorn app.main:app --reload
+#!/usr/bin/env bash
+
+# Get IP of postgres docker container
+postgres_ip=$(
+  docker inspect fastapitest-postgres-1 \
+  | jq -r '.[].NetworkSettings.Networks.fastapitest_default.IPAddress'
+)
+
+# Replace old database IP
+if sed -i -r 's/DATABASE_HOSTNAME=(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"DATABASE_HOSTNAME=$postgres_ip"/ app/.env; then
+  echo "Updated DATABASE_HOSTNAME in app/.env to $postgres_ip"
+fi
+
+# Start the fastapi backend
+POSTGRES_CONNECTION=app/.env uvicorn app.main:app --reload
