3b46ad8cc719bcb08812b373c73430bd7a39dd20
jcardarelli <john@cardarel.li>
2022-01-22 15:07:44 -0800

    Remove get-container-ip.sh file
    This functionality is now handled by the startup script

--- get-container-ip.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-container="$1"
-
-if [[ -z "$container" ]]; then
-  echo 'Please provide the container ID as the first argument'
-  exit 1
-fi
-
-docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container"
