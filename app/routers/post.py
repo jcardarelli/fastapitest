3d07e3d6a263162b4d72222e5f4dd05f183ce468
jcardarelli <john@cardarel.li>
2022-01-30 19:04:16 -0800

    Add search functionality to get users endpoint

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,7 +2,7 @@
 Post functions
 """
 
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
@@ -19,6 +19,7 @@ def get_posts(
     current_user: int = Depends(oauth2.get_current_user),
     limit: int = 10,
     skip: int = 0,
+    search: Optional[str] = "",
 ):
     """
     Get all posts
@@ -26,7 +27,13 @@ def get_posts(
     """
     print(limit)
 
-    posts = db.query(models.Post).limit(limit).offset(skip).all()
+    posts = (
+        db.query(models.Post)
+        .filter(models.Post.title.contains(search))
+        .limit(limit)
+        .offset(skip)
+        .all()
+    )
     return posts
 
 
