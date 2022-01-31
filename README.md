fdc65b4ec89cbc9caa61f96e1b3067b58f488b20
jcardarelli <john@cardarel.li>
2022-01-30 19:50:39 -0800

    Move unused psycopg2 code to readme

--- README.md
+++ README.md
@@ -4,3 +4,32 @@ from this freeCodeCamp.org course: https://youtu.be/0sOvCWFmrtA
 
 ## Restricting ORM filter to only show one user's posts
 See: https://www.youtube.com/watch?v=0sOvCWFmrtA&t=30468s
+
+## Using psycopg2 instead of SQLAlchemy
+This code could be added to app/database.py:
+```
+# This code is optional, since we're using SQL Alchemy ORM, and not the
+# psycopg2 library
+# Setup database connection
+while True:
+    try:
+        conn = psycopg2.connect(
+            host=settings.database_hostname,
+            database=settings.database_name,
+            user=settings.database_username,
+            password=settings.database_password,
+            # Sets up python dict with column name and value
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database connection was successful")
+
+        # break out of while loop if true
+        break
+    except Exception as error:
+        print("Database connection failed")
+        print("Error: ", error)
+
+        # wait before reconnecting
+        time.sleep(2)
+```
--- app/main.py
+++ app/main.py
@@ -3,14 +3,10 @@ FastAPI testing
 """
 
 import os
-import time
 
-import psycopg2
 from fastapi import FastAPI
-from psycopg2.extras import RealDictCursor
 
-from . import models, schemas, utils
-from .config import settings
+from . import models
 from .database import engine
 from .routers import auth, post, user
 
@@ -20,30 +16,6 @@ app = FastAPI()
 
 # Get env vars
 path = os.getenv("POSTGRES_CONNECTION")
-print(path)
-
-# Setup database connection
-while True:
-    try:
-        conn = psycopg2.connect(
-            host=settings.database_hostname,
-            database=settings.database_name,
-            user=settings.database_username,
-            password=settings.database_password,
-            # Sets up python dict with column name and value
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        print("Database connection was successful")
-
-        # break out of while loop if true
-        break
-    except Exception as error:
-        print("Database connection failed")
-        print("Error: ", error)
-
-        # wait before reconnecting
-        time.sleep(2)
 
 
 # Include router objects from routers directory
