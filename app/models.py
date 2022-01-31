00434e2a55d3c8cdce37f69a6891bdf0ba7c2ae1
jcardarelli <john@cardarel.li>
2022-01-30 20:16:10 -0800

    Add votes table via ORM

--- app/models.py
+++ app/models.py
@@ -37,3 +37,15 @@ class User(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    # Column definitions
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+    )
